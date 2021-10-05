c8769fd1217d80c93025bc38adcdc37eea526f12
enzo <enzo.ccc@gmail.com>
2021-10-05 17:08:59 +0200

    removed message box

--- __init__.py
+++ __init__.py
@@ -56,40 +56,39 @@ for l in missing_libraries:
 
 if install_libraries:
     from qgis.PyQt.QtWidgets import QMessageBox
-    if L=='it':
-        res = QMessageBox.warning(None, 'Total Open Station ',
-                              "Se vedi questo messaggio significa che il pacchetto totalopenstation deve essere installato\aggiornato\n\n"
-                              "alla versione modificata. clicca ok per installare", QMessageBox.Ok | QMessageBox.Cancel)
-    
-    else:
-        res = QMessageBox.warning(None, 'Total Open Station ',
-                              "If you see this message it means the required package totalopenstation is missing from your machine:\n\n"
-                              "Do you want install the missing package? Remember you need start QGIS like Admin", QMessageBox.Ok | QMessageBox.Cancel)
-    if res == QMessageBox.Ok:
+    # if L=='it':
+    #     res = QMessageBox.warning(None, 'Total Open Station ',
+    #                           "Se vedi questo messaggio significa che il pacchetto totalopenstation deve essere installato\aggiornato\n\n"
+    #                           "alla versione modificata. clicca ok per installare", QMessageBox.Ok | QMessageBox.Cancel)
+    #
+    # else:
+    #     res = QMessageBox.warning(None, 'Total Open Station ',
+    #                           "If you see this message it means the required package totalopenstation is missing from your machine:\n\n"
+    #                           "Do you want install the missing package? Remember you need start QGIS like Admin", QMessageBox.Ok | QMessageBox.Cancel)
+    # if res == QMessageBox.Ok:
         
 
-        python_path = sys.exec_prefix
-        python_version = sys.version[:3]
-        if platform.system()=='Windows':
-            cmd = '{}\python'.format(python_path)
-        else:
-            cmd = '{}/bin/python{}'.format(python_path, python_version)
-        try:
+    python_path = sys.exec_prefix
+    python_version = sys.version[:3]
+    if platform.system()=='Windows':
+        cmd = '{}\python'.format(python_path)
+    else:
+        cmd = '{}/bin/python{}'.format(python_path, python_version)
+    try:
+        subprocess.call(
+            [cmd, '{}'.format(os.path.join(os.path.dirname(__file__), 'scripts', 'modules_installer.py')),
+             ','.join(install_libraries)], shell=True if platform.system()=='Windows' else False)
+    except Exception as e:
+        if platform.system()=='Darwin':
+            library_path = '/Library/Frameworks/Python.framework/Versions/{}/bin'.format(python_version)
+            cmd = '{}/python{}'.format(library_path, python_version)
             subprocess.call(
                 [cmd, '{}'.format(os.path.join(os.path.dirname(__file__), 'scripts', 'modules_installer.py')),
-                 ','.join(install_libraries)], shell=True if platform.system()=='Windows' else False)
-        except Exception as e:
-            if platform.system()=='Darwin':
-                library_path = '/Library/Frameworks/Python.framework/Versions/{}/bin'.format(python_version)
-                cmd = '{}/python{}'.format(library_path, python_version)
-                subprocess.call(
-                    [cmd, '{}'.format(os.path.join(os.path.dirname(__file__), 'scripts', 'modules_installer.py')),
-                     ','.join(install_libraries)])
-            else:
-                error = traceback.format_exc()
-                QgsMessageLog.logMessage(error, tag="TotalOpenStation", level=Qgis.Critical)
-    else:
-        pass
+                 ','.join(install_libraries)])
+        else:
+            error = traceback.format_exc()
+            QgsMessageLog.logMessage(error, tag="TotalOpenStation", level=Qgis.Critical)
+
 
 def classFactory(iface):  # pylint: disable=invalid-name
     """Load Totalopenstation class from file Totalopenstation.
