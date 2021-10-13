3c724446305b1eb3f4abd40febdced00ae4eec99
enzo <enzo.ccc@gmail.com>
2021-10-13 16:07:23 +0200

    v3.8

--- __init__.py
+++ __init__.py
@@ -38,7 +38,7 @@ try:
     
     import pkg_resources
 
-    pkg_resources.require("totalopenstation==0.5.2.16_dev_enzo")
+    pkg_resources.require("totalopenstation==0.5.2.17_dev_enzo")
     
 
 except Exception as e:
