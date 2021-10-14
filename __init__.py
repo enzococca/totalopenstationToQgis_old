ba9e06174125cee9101c05d693f2ccf03987aa4f
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2021-10-14 09:23:31 +0200

    Remove force installation. fixes #2

--- __init__.py
+++ __init__.py
@@ -25,55 +25,6 @@
 
 
 # noinspection PyPep8Naming
-import os
-import re
-import subprocess
-import sys
-import traceback
-import platform
-from qgis.core import QgsMessageLog, Qgis, QgsSettings
-L=QgsSettings().value("locale/userLocale")[0:2]
-missing_libraries = []
-try:
-    
-    import pkg_resources
-
-    pkg_resources.require("totalopenstation==0.5.2.17_dev_enzo")
-    
-
-except Exception as e:
-    missing_libraries.append(str(e))
-
-install_libraries = []
-for l in missing_libraries:
-    p = re.findall(r"'(.*?)'", l)
-    install_libraries.append(p[0])
-
-if install_libraries:
-
-
-    python_path = sys.exec_prefix
-    python_version = sys.version[:3]
-    if platform.system()=='Windows':
-        cmd = '{}\python'.format(python_path)
-    else:
-        cmd = '{}/bin/python{}'.format(python_path, python_version)
-    try:
-        subprocess.call(
-            [cmd, '{}'.format(os.path.join(os.path.dirname(__file__), 'scripts', 'modules_installer.py')),
-             ','.join(install_libraries)], shell=True if platform.system()=='Windows' else False)
-    except Exception as e:
-        if platform.system()=='Darwin':
-            library_path = '/Library/Frameworks/Python.framework/Versions/{}/bin'.format(python_version)
-            cmd = '{}/python{}'.format(library_path, python_version)
-            subprocess.call(
-                [cmd, '{}'.format(os.path.join(os.path.dirname(__file__), 'scripts', 'modules_installer.py')),
-                 ','.join(install_libraries)])
-        else:
-            error = traceback.format_exc()
-            QgsMessageLog.logMessage(error, tag="TotalOpenStation", level=Qgis.Critical)
-
-
 def classFactory(iface):  # pylint: disable=invalid-name
     """Load Totalopenstation class from file Totalopenstation.
 
